849b9756c922ef809ee946908cfad15b6a7a6c57
Sefa Tohumcu <sefatohumcu87.st@gmail.com>
2022-08-22 17:58:14 +0200

    ilk degisiklik

---
+++ src/Lambda_practice/Methodlarim.java
@@ -0,0 +1,7 @@
+package Lambda_practice;
+public class Methodlarim {//seed (tohum) methodlar create edildi
+    public static boolean ciftElemaniBul(int x){
+        return  x % 2==0;
+        // 2.yol return x % 2!=1;
+    }
+}
